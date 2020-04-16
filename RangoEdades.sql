74521a6e82bcc928c05eeaabd1c7ad4e431e46f6
M1kee <michael.nunezb@outlook.com>
2020-04-16 16:31:37 -0400

    delete table for edad

--- RangoEdades.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[RangoEdades]
-(
-	[Id] INT NOT NULL CONSTRAINT [PK_RangoEdades] PRIMARY KEY,
-	[Descripcion] NVARCHAR(50) NOT NULL,
-	[Desde] INT NOT NULL,
-	[Hasta] INT NOT NULL
-)
